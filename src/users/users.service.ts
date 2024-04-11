640e34de6bd6f3581618fee11b0189e7f7177b69
Kevin Ferreira Feitosa <kevin.feitosa@sistemafiea.com.br>
2024-04-11 10:04:58 -0300

    refactor: removed dependencies

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {

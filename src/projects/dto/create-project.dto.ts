aa1fe902c64d51d7e654d6e5097bf5d51b76e37c
Kevin Ferreira Feitosa <kevin.feitosa@sistemafiea.com.br>
2024-04-11 10:45:32 -0300

    feat: created project dto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,11 @@
-export class CreateProjectDto {}
+import { IsNotEmpty, IsString,  } from "class-validator";
+
+export class CreateProjectDto {
+    @IsString()
+    @IsNotEmpty({ message: "project name can't be empty" })
+    name: string;
+
+    @IsString()
+    @IsNotEmpty({ message: "project descriptio can't be empty" })
+    description: string;
+}
